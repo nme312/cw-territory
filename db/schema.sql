f39239976b388e98ab9e525ce6809ddc28bd31ed
SkyNet <nme312.noreply.github.com>
2020-08-03 10:21:14 -0500

    add lw schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,19 @@ create database cypresswood_db;
 
 use cypresswood_db;
 
-create table territory(
+create table territory_dtd(
+    id int not null auto_increment,
+    territory_number varchar(255),
+    current_possesor varchar(255),
+    checkout_date datetime,
+    due_date datetime,
+    time_overdue datetime,
+    territory_status boolean,
+    territory_rank boolean,
+    primary key (id)
+)
+
+create table territory_lw(
     id int not null auto_increment,
     territory_number varchar(255),
     current_possesor varchar(255),
